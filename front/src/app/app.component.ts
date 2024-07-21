9865191b536b14382b9764bd4a44d364a299e094
karen <ksbs@cin.ufpe.br>
2024-07-21 13:28:04 -0300

    <add> template and styling of occurrences details page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {}
